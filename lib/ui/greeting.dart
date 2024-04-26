ac20b987277c7d4729718c5aef3d22f5ce5829f6
Daffa Aditya Rejasa Ruswanto <e41211358@student.polije.ac.id>
2024-04-26 19:03:41 +0700

    learn Responsive UI

--- lib/ui/greeting.dart
+++ lib/ui/greeting.dart
@@ -12,120 +12,174 @@ class GreetingScreen extends StatelessWidget {
 
   Widget _bodyHome(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 20.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const SizedBox(
-            height: 30.0,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Selamat Datang",
-                    style:
-                        TextStyle(fontSize: 14.0, color: Colors.grey.shade500),
-                  ),
-                  Text(
-                    "Caitlin Halderman",
-                    style:
-                        TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold),
-                  )
-                ],
-              ),
-              Container(
-                width: 40,
-                height: 40,
-                decoration: BoxDecoration(
-                    color: Colors.grey.shade100,
-                    borderRadius: BorderRadius.circular(10)),
-                child: Icon(
-                  Icons.widgets,
-                  color: Colors.pink.shade300,
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 30.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Selamat Datang",
+                      style: TextStyle(
+                          fontSize: 14.0, color: Colors.grey.shade500),
+                    ),
+                    const Text(
+                      "Caitlin Halderman",
+                      style: TextStyle(
+                          fontSize: 22.0, fontWeight: FontWeight.bold),
+                    )
+                  ],
                 ),
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Stack(
-            alignment: Alignment.bottomLeft,
-            children: [
-              Container(
-                width: double.infinity,
-                decoration: BoxDecoration(
+                Container(
+                  width: 40,
+                  height: 40,
+                  decoration: BoxDecoration(
+                      color: Colors.grey.shade100,
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Icon(
+                    Icons.widgets,
                     color: Colors.pink.shade300,
-                    borderRadius: BorderRadius.circular(20.0)),
-                child: const Padding(
-                  padding: EdgeInsets.only(left: 150, top: 30, bottom: 50),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Big Sale",
-                        style: TextStyle(
-                            fontSize: 22.0,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        "Dapatkan harga murah banget , mumpung lagi promo hari buruh nasional!!",
-                        style: TextStyle(color: Colors.white),
-                      )
-                    ],
                   ),
                 ),
-              ),
-              Image.asset(
-                "assets/model.png",
-                width: 230,
-                height: 220,
-              )
-            ],
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          const Text(
-            "Menu Informasi",
-            style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-          ),
-          const Text("Soon Available This Fiture"),
-          ElevatedButton(
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (context) {
-                    return AlertDialog(
-                      title: const Text("Konfirmasi"),
-                      content: const Text(
-                          "Yakin ingin menolak cinta saya kamu? gak nyesell?"),
-                      actions: [
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                          child: const Text("Iya"),
+              ],
+            ),
+            const Text(
+              "tidak",
+              style: TextStyle(fontWeight: FontWeight.w700, fontSize: 13),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Stack(
+              alignment: Alignment.bottomLeft,
+              children: [
+                Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      color: Colors.pink.shade300,
+                      borderRadius: BorderRadius.circular(20.0)),
+                  child: const Padding(
+                    padding: EdgeInsets.only(left: 150, top: 30, bottom: 50),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Big Sale",
+                          style: TextStyle(
+                              fontSize: 22.0,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold),
                         ),
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                          child: const Text("Tidakkk"),
+                        Text(
+                          "Dapatkan harga murah banget , mumpung lagi promo hari buruh nasional!!",
+                          style: TextStyle(color: Colors.white),
                         )
                       ],
-                    );
-                  },
-                );
+                    ),
+                  ),
+                ),
+                Image.asset(
+                  "assets/model.png",
+                  width: 230,
+                  height: 220,
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              "Menu Informasi",
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+            const Text(
+              "Promo Sale Baju! Besar besaran cuy buruann",
+              style: TextStyle(fontSize: 14.0),
+            ),
+            LayoutBuilder(
+              builder: (context, constraints) {
+                if (constraints.maxWidth < 600) {
+                  return GridView.count(
+                    crossAxisCount: 2,
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    children: _generateWidget(),
+                  );
+                } else if (constraints.maxWidth < 900) {
+                  return GridView.count(
+                    crossAxisCount: 3,
+                    shrinkWrap: true,
+                    children: _generateWidget(),
+                  );
+                } else {
+                  return GridView.count(
+                    crossAxisCount: 6,
+                    shrinkWrap: true,
+                    children: _generateWidget(),
+                  );
+                }
               },
-              child: const Text("Coba Tekan Saya"))
-        ],
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: const Text("Konfirmasi"),
+                        content: const Text(
+                            "Yakin ingin menolak cinta saya kamu? gak nyesell?"),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: const Text("Iya"),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: const Text("Tidakkk"),
+                          )
+                        ],
+                      );
+                    },
+                  );
+                },
+                child: const Text("Coba Tekan Saya"))
+          ],
+        ),
       ),
     );
+  }
+
+  List<Widget> _generateWidget() {
+    return List<Widget>.generate(10, (index) {
+      return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          height: 200,
+          decoration: BoxDecoration(
+              color: Colors.pink.shade300,
+              borderRadius: BorderRadius.circular(15)),
+          child: const Center(
+            child: Text(
+              "TES WIDGET",
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
+            ),
+          ),
+        ),
+      );
+    });
   }
 }
--- lib/ui/menu.dart
+++ lib/ui/menu.dart
@@ -5,11 +5,81 @@ class MenuScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+    Orientation getOrientation = MediaQuery.of(context).orientation;
+
     return Scaffold(
-      body: Column(
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          if (constraints.maxWidth < 600) {
+            return ListView(
+              children: _generateWidget(),
+            );
+          } else if (constraints.maxWidth < 900) {
+            return GridView.count(
+              crossAxisCount: 2,
+              children: _generateWidget(),
+            );
+          } else {
+            return GridView.count(
+              crossAxisCount: 6,
+              children: _generateWidget(),
+            );
+          }
+        },
+      ),
+    );
+  }
+
+  List<Widget> _generateWidget() {
+    return List<Widget>.generate(10, (index) {
+      return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          color: Colors.pink,
+          height: 200,
+        ),
+      );
+    });
+  }
+
+  Center _body(
+      Size screenSize, Orientation getOrientation, BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          ElevatedButton(onPressed: () {}, child: Text("data")),
-          Badge()
+          Text(
+              "Size With : ${screenSize.width.toStringAsFixed(2)}  & Size Height : ${screenSize.height.toStringAsFixed(2)}"),
+          Text("Screen Orientation : $getOrientation"),
+          ElevatedButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: const Text("Konfirmasi"),
+                      content: const Text(
+                          "Yakin ingin menolak cinta saya kamu? gak nyesell?"),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: const Text("Iya"),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: const Text("Tidakkk"),
+                        )
+                      ],
+                    );
+                  },
+                );
+              },
+              child: const Text("Coba Tekan Saya"))
         ],
       ),
     );
